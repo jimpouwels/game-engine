7e99142905f9522832a637f466e3b1cd42a47874
Jim Pouwels <jim.pouwels@gmail.com>
2022-02-01 19:44:32 +0100

    handle asteroid movement by angle

--- game/asteroid.cpp
+++ game/asteroid.cpp
@@ -1,17 +1,17 @@
 #include <math.h>
 #include "asteroid.hpp"
 #include "screen.hpp"
+#include "geo2D.hpp"
 
 #include <iostream>
 
-const int Asteroid::SPEED_IN_PIXELS_PER_SECOND = 150;
+const int Asteroid::SPEED_IN_PIXELS_PER_SECOND = 500;
 const int Asteroid::ROTATION_DEGREES_PER_SECOND = 30;
 
 Asteroid::Asteroid(jimp::Screen* screen, float x, float y) : jimp::AnimatedSprite(screen, x, y, 0.1F, -1) {
     addSprite("default", "asteroid.png");
     setRotationAngle(90);
-    directionX = DirectionX::RIGHT;
-    directionY = DirectionY::DOWN;
+    directionAngle = 135;
 }
 
 Asteroid::~Asteroid() {
@@ -19,38 +19,55 @@ Asteroid::~Asteroid() {
 }
 
 void Asteroid::update(float elapsedTime) {
-    float deltaPosition = (SPEED_IN_PIXELS_PER_SECOND) / (1.0F / elapsedTime);
-    
+    handleDirection(elapsedTime);
+    handleMovement(elapsedTime);
+
+    this->updateAnimation(elapsedTime);
+}
+
+void Asteroid::handleDirection(float elapsedTime) {
     if (isOutsideScreenAbove()) {
-        directionY = DirectionY::DOWN;
+        if (directionAngle == 0 || directionAngle == 180) {
+            directionAngle += 180;
+        } else if (directionAngle < 90) {
+            directionAngle += 90;
+        } else {
+            directionAngle -= 90;
+        }
     } else if (isOutsideScreenBelow()) {
-        directionY = DirectionY::UP;
+        if (directionAngle == 0 || directionAngle == 180) {
+            directionAngle += 180;
+        } else if (directionAngle > 180) {
+            directionAngle += 90;
+        } else {
+            directionAngle -= 90;
+        }
     }
     if (isOutsideScreenLeft()) {
-        directionX = DirectionX::RIGHT;
+        if (directionAngle == 270 || directionAngle == 90) {
+            directionAngle += 180;
+        } else if (directionAngle > 270) {
+            directionAngle += 90;
+        } else {
+            directionAngle -= 90;
+        }
     } else if (isOutsideScreenRight()) {
-        directionX = DirectionX::LEFT;
+        if (directionAngle == 270 || directionAngle == 90) {
+            directionAngle += 180;
+        } else if (directionAngle < 90) {
+            directionAngle -= 90;
+        } else {
+            directionAngle += 90;
+        }
     }
-    
-    switch (directionY) {
-        case UP:
-            setY(getPosition().y - deltaPosition);
-            break;
-        case DOWN:
-            setY(getPosition().y + deltaPosition);
-            break;
-    }
-    switch (directionX) {
-        case RIGHT:
-            setX(getPosition().x + deltaPosition);
-            break;
-        case LEFT:
-            setX(getPosition().x - deltaPosition);
-            break;
-    }
+    directionAngle = jimp::Geo2D::normalizeAngle(directionAngle);
+}
+
+void Asteroid::handleMovement(float elapsedTime) {
+    float distance = (SPEED_IN_PIXELS_PER_SECOND) / (1.0F / elapsedTime);
+    jimp::Point2D deltaPosition = jimp::Geo2D::deltaByAngle(directionAngle, distance);
+    addToPosition(deltaPosition);
 
     float deltaDegrees = ROTATION_DEGREES_PER_SECOND / (1.0F / elapsedTime);
     setRotationAngle(getRotationAngle() + deltaDegrees);
-
-    this->updateAnimation(elapsedTime);
 }
--- game/asteroid.hpp
+++ game/asteroid.hpp
@@ -7,12 +7,11 @@
 class Asteroid : public jimp::AnimatedSprite {
   
 private:
-    enum DirectionX { RIGHT, LEFT };
-    enum DirectionY { UP, DOWN };
     static const int SPEED_IN_PIXELS_PER_SECOND;
     static const int ROTATION_DEGREES_PER_SECOND;
-    DirectionX directionX;
-    DirectionY directionY;
+    float directionAngle;
+    void handleDirection(float elapsedTime);
+    void handleMovement(float elapsedTime);
     
 public:
     Asteroid(jimp::Screen* screen, float x, float y);
