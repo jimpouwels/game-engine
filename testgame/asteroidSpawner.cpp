45ccaf08717214599a0e68cb01204de322ab2c60
Jim Pouwels <jim.pouwels@gmail.com>
2022-02-13 14:20:35 +0100

    fixed asteroids

--- testgame/asteroidSpawner.cpp
+++ testgame/asteroidSpawner.cpp
@@ -2,7 +2,7 @@
 #include "geo2D.hpp"
 #include "mathUtils.hpp"
 
-const uint8_t AsteroidSpawner::SECONDS_BETWEEN_ASTEROID_SPAWN = 300;
+const uint8_t AsteroidSpawner::SECONDS_BETWEEN_ASTEROID_SPAWN = 3;
 
 AsteroidSpawner::AsteroidSpawner(jimp::GameEngine* gameEngine) {
     asteroids = new std::vector<Asteroid*>;
@@ -18,25 +18,20 @@ AsteroidSpawner::~AsteroidSpawner() {
 
 void AsteroidSpawner::onUpdate(float elapsedTime) {
     timeSinceLastAsteroid += elapsedTime;
-    
-    if (asteroids->size() == 0) {
-        asteroids->push_back(new Asteroid(gameEngine, 50, 500, 90));
-        asteroids->push_back(new Asteroid(gameEngine, 1900, 500, 270));
-    }
     if (timeSinceLastAsteroid >= SECONDS_BETWEEN_ASTEROID_SPAWN) {
-//        timeSinceLastAsteroid = 0.0F;
-//
-//        float x, angle = 0.0F;
-//        float y = jimp::MathUtils::randomNumberBetween(gameEngine->getScreenHeight() * 0.25, gameEngine->getScreenHeight() * 0.75);
-//        int randomSide = jimp::MathUtils::randomNumberBetween(0, 2);
-//        if (randomSide == 0) { // left
-//            x = -100;
-//            angle = jimp::MathUtils::randomNumberBetween(45, 135);
-//        } else { // right
-//            x = gameEngine->getScreenWidth() + 100;
-//            angle = jimp::MathUtils::randomNumberBetween(225, 315);
-//        }
-//        asteroids->push_back(new Asteroid(gameEngine, x, y, angle));
+        timeSinceLastAsteroid = 0.0F;
+
+        float x, angle = 0.0F;
+        float y = jimp::MathUtils::randomNumberBetween(gameEngine->getScreenHeight() * 0.25, gameEngine->getScreenHeight() * 0.75);
+        int randomSide = jimp::MathUtils::randomNumberBetween(0, 2);
+        if (randomSide == 0) { // left
+            x = -100;
+            angle = jimp::MathUtils::randomNumberBetween(45, 135);
+        } else { // right
+            x = gameEngine->getScreenWidth() + 100;
+            angle = jimp::MathUtils::randomNumberBetween(225, 315);
+        }
+        asteroids->push_back(new Asteroid(gameEngine, x, y, angle));
     }
     std::list<Asteroid*> asteroidsToDelete;
     for (const auto& asteroid: *asteroids) {
