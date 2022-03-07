bdcf42061dee4324550e2521364c931ced7b1dc9
Jim Pouwels <jim.pouwels@gmail.com>
2022-03-08 00:03:29 +0100

    collision

--- mario/plant.cpp
+++ mario/plant.cpp
@@ -5,7 +5,7 @@
 namespace mario {
   
 Plant::Plant(jimp::Vector2D position, uint16_t repeat) : jimp::AnimatedGraphic(position, 0.5F, 0.0F, 0.05F, false) {
-    this->setCollidable(true);
+    this->setCollidable(false);
     this->repeat = repeat;
     for (int i = 0; i < 90; i++) {
         addSprite("default", "Plant3_" + std::to_string(i) + ".png", 1);
