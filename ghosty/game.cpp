8f6b4f726545c84f1ddfb827b084f1bb9985c915
Jim Pouwels <jim.pouwels@gmail.com>
2022-03-18 12:18:32 +0100

    removing commented out things that are now part of the level def

--- ghosty/game.cpp
+++ ghosty/game.cpp
@@ -16,26 +16,11 @@ private:
 public:
     GhostyGame(int screenWidth, int screenHeight, std::string name) : GameEngine(screenWidth, screenHeight, 5000, name, 1000, true) {
         setBackgroundColor(0x3ba7b3);
- 
-//        jimp::Vector2D flowerPosition = jimp::Vector2D::from(platform->getPosition().x + platform->getWidth() - 40, platform->getPosition().y + platform->getHeight() / 2 - 256);
-//        Decoration* flower = new Decoration(flowerPosition, "BlueFlower_{i}.png", 60, 90, 0.5F, 4);
-//
-//        Decoration* plantSmall = new Decoration(jimp::Vector2D::from(platform->getPosition().x + platform->getWidth() - 130, platform->getPosition().y - 55 ), "plant-small.png", 1, 0, 0.2F, 4);
-      
-        // SECOND SMALL PLATFORM
-//        Platform* platform2 = new Platform("platform-top-left.png", "platform-top-middle.png", 1, 4, 512, 0.3F, jimp::Vector2D::from(platform->getPosition().x + platform->getWidth() + 300, 2100));
-//
-//        Decoration* plantPoison1 = new Decoration(jimp::Vector2D::from(platform2->getPosition().x + 100, platform2->getPosition().y + 75), "PlantPosion_{i}.png", 30, 180, 0.3F, 4);
-//        Decoration* rock = new Decoration(jimp::Vector2D::from(platform2->getPosition().x + 200, platform2->getPosition().y - 110), "rock1.png", 1, 0, 0.25F, 4);
-        
+
         // THIRD SMALL PLATFORM
 //        Platform* platform3 = new Platform("platform-top-left.png", "platform-top-middle.png", 1, 2, 512, 0.3F, jimp::Vector2D::from(platform2->getPosition().x + 820, platform2->getPosition().y + platform2->getHeight() + 200 ));
         
         // BACKGROUND
-//        Decoration* blackPlant = new Decoration(jimp::Vector2D::from(platform->getPosition().x + platform->getWidth() + 375, platform->getPosition().y + platform->getHeight() - 520), "Plant3_{i}.png", 90, 0, 1.0F, 4);
-//        blackPlant->setRgbLevels(jimp::Color::black());
-//        blackPlant->animateRgbLevels(jimp::Color::from(90, 90, 90), 1);
-//
 //        Decoration* rock1 = new Decoration(jimp::Vector2D::from(platform->getPosition().x + 700, platform->getPosition().y - 200), "rock1-background.png", 1, 0, 1.7F, 4);
 //        Decoration* spikes = new Decoration(jimp::Vector2D::from(-300, platform->getPosition().y - 300), "spikes1.png", 1, 45, 0.8F, 5);
 //        Decoration* hill = new Decoration(jimp::Vector2D::from(-250, platform->getPosition().y + 300), "hill.png", 1, 0, 0.8F, 1);
